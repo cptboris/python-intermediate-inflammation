150177380d82a0c350f9b1815ad07cfd61fa6dd6
Joao Vargas <joao.francisco.vargas@gmail.com>
2024-10-01 12:16:08 +0200

    Improved robustness of patient_normalise routine

--- tests/test_models.py
+++ tests/test_models.py
@@ -30,6 +30,7 @@ def test_daily_mean_integers():
     # Need to use Numpy testing functions to compare arrays
     npt.assert_array_equal(daily_mean(test_input), test_result)
 
+
 @pytest.mark.parametrize(
     "test, expected",
     [
@@ -43,6 +44,7 @@ def test_daily_max_integers(test,expected):
     from inflammation.models import daily_max
     # Assert the comparison of arrays
     npt.assert_array_equal(daily_max(test),expected)
+
 
 @pytest.mark.parametrize(
     "test, expected",
@@ -64,3 +66,47 @@ def test_daily_min_string():
 
     with pytest.raises(TypeError):
         error_expected = daily_min([['Hello', 'there'], ['General', 'Kenobi']])
+
+
+@pytest.mark.parametrize(
+    "test, expected, expect_raises",
+    [
+        (
+            [[0, 0, 0], [0, 0, 0], [0, 0, 0]], 
+            [[0, 0, 0], [0, 0, 0], [0, 0, 0]],
+            None
+        ),
+        (
+            [[1, 1, 1], [1, 1, 1], [1, 1, 1]], 
+            [[1, 1, 1], [1, 1, 1], [1, 1, 1]],
+            None
+        ),
+        (
+            [[1, 2, 3], [4, 5, 6], [7, 8, 9]], 
+            [[0.33, 0.67, 1], [0.67, 0.83, 1], [0.78, 0.89, 1]],
+            None
+        ),
+        (
+            [[-1, -1, 1], [-2,float('nan'), 2], [float('nan'), float('nan'), 3]], 
+            [[0, 0, 1], [0, 0, 1], [0, 0, 1]],
+            ValueError,
+        ),
+        (
+            [[-1, -1, -1], [-2,-2, -2], [-3, -3, -3]], 
+            [[0, 0, 0], [0, 0, 0], [0, 0, 0]],
+            ValueError
+        ),
+    ])
+
+def test_patient_normalise(test, expected, expect_raises):
+    """Test normalisation works for arrays of one and positive integers.
+       Test with a relative and absolute tolerance of 0.01."""
+    from inflammation.models import patient_normalise
+    
+    if expect_raises is not None:
+        with pytest.raises(expect_raises):
+            result = patient_normalise(np.array(test))
+            npt.assert_allclose(result, np.array(expected), rtol=1e-2, atol=1e-2)
+    else:
+        result = patient_normalise(np.array(test))
+        npt.assert_allclose(result, np.array(expected), rtol=1e-2, atol=1e-2)
