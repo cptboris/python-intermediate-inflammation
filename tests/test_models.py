ae84a0898a65952315e608367ffe4a6e54458f8d
Joao Vargas <joao.francisco.vargas@gmail.com>
2024-10-15 11:13:52 +0200

    Fixed name of daily_std_dev function and applied changes throughout. Fixed some code readability issues

--- tests/test_models.py
+++ tests/test_models.py
@@ -39,5 +39,5 @@ def test_daily_mean_integers():
 ])
 def test_daily_standard_deviation(data, expected_standard_deviation):
     from inflammation.models import s_dev
-    result_data = s_dev(data)['standard deviation']
+    result_data = daily_std_dev(data)
     npt.assert_approx_equal(result_data, expected_standard_deviation)
